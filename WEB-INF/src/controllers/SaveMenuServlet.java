929ee298ad3490ef9f56d9cdc60241aceab22320
tornado1512 <vagrahari943@gmail.com>
2019-10-08 13:33:46 +0530

    addMenuServlet

--- WEB-INF/src/controllers/SaveMenuServlet.java
+++ WEB-INF/src/controllers/SaveMenuServlet.java
@@ -5,8 +5,18 @@ import javax.servlet.*;
 import java.io.*;
 
 public class SaveMenuServlet extends HttpServlet{
-	public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException,ServletException{
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException,ServletException{
 		HttpSession session=request.getSession();
+		String [] prices = request.getParameterValues("price1");
+		String [] items = request.getParameterValues("item1");
+		System.out.println(prices.length);
+		System.out.println(items.length);
+		for(String price:prices){
+			System.out.println("p:"+price);
+		}
+		for(String item:items){
+			System.out.println("i:"+item);
+		}
 
 	}
 }
